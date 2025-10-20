39bd93c17c10ffa33f8aa73e3419f73e5333cdc1
Fernando <>
2025-10-20 17:46:48 +0200

    Cambiar Scripts y ajustes

---
+++ PRUEBA_FS_UNITY_01/Assets/Script/movimiento.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+ [RequireComponent(typeof(Rigidbody2D))]
+public class movimiento : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float Velocidad = 3;
+    public float Movimiento;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Llamada al RigidBody2D
+      rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimietno Horizaontal
+        Movimiento = Input.GetAxisRaw("Horizontal");
+
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(Movimiento * Velocidad, rb.linearVelocity.y * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        if (collider.gameObject.CompareTag("Daño"))
+        {
+            Destroy(collider.gameObject);
+        }
+
+    }
+
+}
